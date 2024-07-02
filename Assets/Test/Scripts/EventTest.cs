ab076dad6385cba027db701733e541262978413e
OGYWORLD <ogy1004@naver.com>
2024-07-02 19:07:44 +0900

    20240702

---
+++ Assets/Test/Scripts/EventTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class EventTest : MonoBehaviour
+{
+    private XRBaseControllerInteractor interactor;
+    private int count = 0;
+
+    private void Awake()
+    {
+        interactor = GetComponent<XRBaseControllerInteractor>();
+        interactor.selectEntered.AddListener(
+            (args) => {
+                XREventCall(args, count++);
+            }
+            );
+
+    }
+
+    public void XREventCall(BaseInteractionEventArgs args, int count)
+    {
+        print($"{args.interactorObject.transform.name} called {count} times.");
+    }
+
+
+}
